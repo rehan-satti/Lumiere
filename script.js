593acfa8807e2dd7cd1ee00efb16e2e857238ade
rehan-satti <rsatti718@gmail.com>
2026-06-20 09:35:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -179,7 +179,7 @@ window.addEventListener('load', () => {
         if (isHomePage && (!loaderShown || isReload)) {
             setTimeout(() => {
                 loader.style.opacity = '0';
-                setTimeout(() => loader.style.display = 'none', 800);
+                setTimeout(() => loader.style.display = 'none', 1000);
             }, 1500);
         }
     }
