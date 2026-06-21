d2cb44207e870d383adc6d9bfc04082fda1462dd
rehan-satti <rsatti718@gmail.com>
2026-06-21 09:36:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -181,7 +181,7 @@ window.addEventListener('load', () => {
             setTimeout(() => {
                 loader.style.opacity = '0';
                 setTimeout(() => loader.style.display = 'none', 900);
-            }, 3600);
+            }, 4000);
         }
     }
     sessionStorage.setItem('lumiereLoaderShown', 'true');
